3f72230cc7fcaefbebf8786cefea3cb46acb43bd
abhi-rout <130905508+abhi-rout@users.noreply.github.com>
2023-07-06 19:09:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
- let height=this.innerHeight;
-    let width=this.innerWidth;
-   let div=document.getElementById("sizeInfo");
-    let h=div.firstChild;
-    h.innerText="Width: "+width+" and Height: "+height;  
+ // let height=this.innerHeight;
+ //    let width=this.innerWidth;
+ //   let div=document.getElementById("sizeInfo");
+ //    let h=div.firstChild;
+ //    h.innerText="Width: "+width+" and Height: "+height;  
 window.addEventListener("resize",function(){
     let height=this.innerHeight;
     let width=this.innerWidth;
